17942562f416f281d1a3f9ab03cc89b0fc66f5b0
Tushar Nadavadekar <yedveshubham@gmail.com>
2024-10-05 13:40:54 +0530

    solved-java-easy-15
    Hack o' Holics
    java/easy/java-15

--- Java/Easy/15.java
+++ Java/Easy/15.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        boolean isValid = 0;
+        int isValid = 0;   // boolean vaslue can only contain true or false a vlaue
         System.out.println(isValid);
     }
 }
