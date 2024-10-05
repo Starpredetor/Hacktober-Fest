6c00cf3b2a96432f394a9e4fbb053d9c01655f77
Omkar Narkar <yedveshubham@gmail.com>
2024-10-05 13:50:25 +0530

    cpp_21
    Hack O Holics
    C++/Easy/C++_21

--- C++/Easy/21.cpp
+++ C++/Easy/21.cpp
@@ -3,8 +3,11 @@ using namespace std;
 int main()
 {
     int a = 10, b = 20;
-    if (a = b)
+    if (a == b) {
         cout << "Equal";
+    }
     else
         cout << "Not Equal";
+
+    return 0;
 }
