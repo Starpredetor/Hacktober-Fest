547274c8f5ce17ab0c941ed07351ec4a8260a924
Tushar Nadavadekar <yedveshubham@gmail.com>
2024-10-05 13:49:07 +0530

    solved-java-easy-16
    Hack o' Holics
    java/easy/java-16

--- Java/Easy/16.java
+++ Java/Easy/16.java
@@ -2,7 +2,10 @@ class Main {
     public static void main(String[] args) {
         int a = 5;
         int b = 10;
-        System.out.println(a > b ? "Greater" : "Smaller");
-        else System.out.println("Equal");
+        if(a != b) {
+            System.out.println(a > b ? "Greater" : "Smaller");
+        } else {
+            System.out.println("Equal");
+        }
     }
 }
