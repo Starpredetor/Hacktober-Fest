aa845bf069bb8f087412340059b46a04edd15601
Starpredetor <yedveshubham@gmail.com>
2024-10-05 13:33:48 +0530

    Solve py 3
    Hack O' Holics
    python/easy/py 3

--- Python/Easy/3.py
+++ Python/Easy/3.py
@@ -8,3 +8,10 @@ class Car:
     def __init__(self, make, model):
         self.make = make
         self.model = model
+
+greet()
+print(add(10,20))
+car = Car("BMW", "S Class")
+print(car.make)
+print(car.model)
+
