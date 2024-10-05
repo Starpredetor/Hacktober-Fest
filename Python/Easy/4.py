4a20b65aee8467c34e2417d3a5f440e01f00b76e
Starpredetor <yedveshubham@gmail.com>
2024-10-05 13:44:51 +0530

    Solve py 4
    Hack O' Holics
    python/easy/ py 4

--- Python/Easy/4.py
+++ Python/Easy/4.py
@@ -1,9 +1,9 @@
-message = ‘py" 
+message = "py" # fixed string 
 print(message)
 
-title = "Python's "cool" feature"
+title = "Python's \"cool\" feature"   # fixed string added \ escape sequence
 
 description = 'This is a Python "error example'
 
-sentence = “Hello, Python!”
+sentence = "Hello, Python!"  # fixed string
 print(sentence)
