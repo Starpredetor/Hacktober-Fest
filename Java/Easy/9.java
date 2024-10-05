9476da7aa96d3d81ec2be6a225a08a44422f2a03
Tushar Nadavadekar <143600822+TokiG0@users.noreply.github.com>
2024-10-05 12:53:32 +0530

    solved-java-easy-9
    Hack o' Holics
    java/easy/java-9

--- Java/Easy/9.java
+++ Java/Easy/9.java
@@ -2,6 +2,8 @@ class Main {
     public static void main(String[] args) {
         int[] numbers = new int[3];
         numbers[0] = 5;
-        numbers[3] = 10;
+        numbers[1] = 10;
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
     }
 }
