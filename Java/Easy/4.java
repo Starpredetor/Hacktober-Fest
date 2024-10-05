f29461f441a28438fad5e282d494ed8b7eec8a50
Tushar Nadavadekar <143600822+TokiG0@users.noreply.github.com>
2024-10-05 12:27:05 +0530

    solved-java-easy-4

--- Java/Easy/4.java
+++ Java/Easy/4.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Main {
     public static void main(String[] args) {
         List<String> names = new ArrayList<>();
         names.add("Alice");
-        System.out.println(names[0]);
+        System.out.println(names.get(0));
     }
 }
